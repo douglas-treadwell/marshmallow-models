4df34523eb5f95a80b0b90d3876a1ec9445e8f46
Douglas Treadwell <douglas.treadwell@gmail.com>
2017-06-11 19:28:13 -0700

    Enabling inheritance and differentiating metaclass constructor attrs.

--- marshmallow_models/models.py
+++ marshmallow_models/models.py
@@ -1,24 +1,39 @@
 from marshmallow.schema import Schema, SchemaMeta
+from marshmallow.fields import FieldABC
 from compat import with_metaclass
+
+
+def is_schema_attribute(attr):
+    return isinstance(attr, FieldABC)
 
 
 class ModelMeta(type):
     def __new__(mcs, name, bases, attrs):
-        model_class = super(ModelMeta, mcs).__new__(mcs, name, bases, attrs)
+        model_attrs = {attr_name: attr
+                       for attr_name, attr in attrs.items()
+                       if not is_schema_attribute(attr)}
 
-        _schema = SchemaMeta('%sSchema' % name, (Schema,), attrs)
+        model_class = super(ModelMeta, mcs).__new__(mcs, name, bases, model_attrs)
 
-        model_class._schema = _schema
+        schema_base = getattr(model_class, '_schema_class', Schema)
+
+        schema_attrs = {attr_name: attr
+                        for attr_name, attr in attrs.items()
+                        if is_schema_attribute(attr)}
+
+        _schema_class = SchemaMeta('%sSchema' % name, (schema_base,), schema_attrs)
+
+        model_class._schema_class = _schema_class
 
         return model_class
 
 
 class Model(with_metaclass(ModelMeta, object)):
-    def __init__(self, raw_data=None, **kwargs):
-        if not isinstance(raw_data, (dict, type(None))):
+    def __init__(self, _raw_data=None, **kwargs):
+        if not isinstance(_raw_data, (dict, type(None))):
             raise ValueError('%s constructor accepts dictionary or kwargs.')
 
-        self.__dict__ = raw_data or kwargs
+        self.__dict__ = _raw_data or kwargs
 
     def validate(self):
-        return self._schema(strict=True).validate(self.__dict__)
+        return self._schema_class(strict=True).validate(self.__dict__)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,12 +1,31 @@
 from unittest import TestCase
+from marshmallow import Schema
 from marshmallow_models import Model
 from marshmallow.fields import String, Integer
 from marshmallow.exceptions import ValidationError
 
 
+# Schemas are here only to compare with Model schemas
+
+class PersonSchema(Schema):
+    name = String(required=True)
+    age = Integer(required=True)
+
+
+class ParentSchema(PersonSchema):
+    num_children = Integer(required=True)
+
+
 class PersonModel(Model):
     name = String(required=True)
     age = Integer(required=True)
+
+    def __str__(self):
+        return '%s, %s' % (self.name, self.age)
+
+
+class ParentModel(PersonModel):
+    num_children = Integer(required=True)
 
 
 class TestModel(TestCase):
@@ -43,3 +62,27 @@ class TestModel(TestCase):
     def test_invalid_constructor_argument(self):
         with self.assertRaises(ValueError):
             PersonModel([])
+
+    def test_model_methods(self):
+        person = PersonModel(dict(name='Tester', age=100))
+
+        self.assertEqual(str(person), 'Tester, 100')
+
+    def test_inheritance(self):
+        person = ParentModel(dict(name='Tester', age=100, num_children=1))
+
+        self.assertIsNotNone(ParentSchema._declared_fields['name'])
+        self.assertIsNotNone(person._schema_class._declared_fields['name'])
+
+        self.assertEqual(person.name, 'Tester')
+        self.assertEqual(person.age, 100)
+        self.assertEqual(person.num_children, 1)
+
+        person.validate()
+
+        person = ParentModel(dict(num_children=1))
+
+        self.assertIsNotNone(person._schema_class._declared_fields['name'])
+
+        with self.assertRaises(ValidationError):  # missing name and age fields
+            person.validate()
